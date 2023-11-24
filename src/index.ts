20d2a49d0b9d422e5d2090285ee5f4e36de12c03
Wolfsin <choose.bc.wolf@gmail.com>
2023-11-24 21:53:22 +0900

    feat: Add a call to getObject with sourceFileList in s3Zip function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { PassThrough, Readable } from 'stream';
 import { S3Client } from '@aws-sdk/client-s3';
-import { listObjects } from './s3Wrapper';
+import { getObject, listObjects } from './s3Wrapper';
+import archiver from 'archiver';
 
 export const s3Zip = async (
     sourceBucket: string,
@@ -11,9 +13,21 @@ export const s3Zip = async (
     zipOptions: object = {},
     s3ClientOptions: object = {},
 ) => {
+    const streamArchiver = archiver('zip', zipOptions);
+    const outputStream = new PassThrough();
     const client = new S3Client(s3ClientOptions);
+
     if (sourceFileList.length === 0) {
         const listObjectResult = await listObjects(client, sourceBucket, sourcePath);
         sourceFileList = [...listObjectResult];
     }
+
+    const fileStreamList = await Promise.all(
+        sourceFileList.map((fileName) => getObject(client, sourceBucket, sourcePath, fileName)),
+    );
+    fileStreamList.forEach((fileStream) => {
+        const name = sourceFileList[fileStreamList.indexOf(fileStream)];
+        streamArchiver.append(fileStream as Readable, { name });
+    });
+    await streamArchiver.finalize();
 };

8ee77bc3d4cf385a071fbe2c14a92258a3eefe93
Wolfsin <choose.bc.wolf@gmail.com>
2023-12-02 21:19:59 +0900

    fix: Fix test failure in action by mocking @aws-sdk/lib-storage

--- __tests__/s3Wrapper.spec.ts
+++ __tests__/s3Wrapper.spec.ts
@@ -1,20 +1,30 @@
-import {
-    S3Client,
-    ListObjectsV2Command,
-    GetObjectCommand,
-    CreateMultipartUploadCommand,
-    UploadPartCommand,
-    PutObjectCommand,
-} from '@aws-sdk/client-s3';
+import { S3Client, ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/client-s3';
 import { mockClient } from 'aws-sdk-client-mock';
 import 'aws-sdk-client-mock-jest';
 import { sdkStreamMixin } from '@smithy/util-stream';
 import { Readable } from 'stream';
 
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { listObjects, getObjectStream, uploadObject } from '../src/s3Wrapper';
 
 const s3Mock = mockClient(S3Client);
+const mocks = vi.hoisted(() => {
+    return {
+        mockUpload: vi.fn(),
+        mockUploadDone: vi.fn(),
+    };
+});
+vi.mock('@aws-sdk/lib-storage', async (importOriginal) => {
+    const actual = await importOriginal<typeof import('@aws-sdk/lib-storage')>();
+    return {
+        ...actual,
+        Upload: mocks.mockUpload.mockImplementation(() => {
+            return {
+                done: mocks.mockUploadDone,
+            };
+        }),
+    };
+});
 describe('s3Wrapper', () => {
     describe('listObjects', () => {
         beforeEach(() => {
@@ -133,28 +143,26 @@ describe('s3Wrapper', () => {
         stream.push(null);
         const sdkStream = sdkStreamMixin(stream);
 
-        beforeEach(() => {
-            s3Mock.on(CreateMultipartUploadCommand).resolves({ UploadId: '1' });
-            s3Mock.on(UploadPartCommand).resolves({ ETag: '1' });
-            s3Mock.on(PutObjectCommand).resolves({});
-        });
-        afterEach(() => {
-            s3Mock.reset();
-        });
         it('file directly below bucket', async () => {
             await uploadObject(new S3Client({}), 'bucket', '', 'test.zip', sdkStream);
-            expect(s3Mock.commandCalls(PutObjectCommand)[0].args[0].input).toEqual({
-                Bucket: 'bucket',
-                Key: 'test.zip',
-                Body: expect.any(Object),
+            expect(mocks.mockUpload).toHaveBeenCalledWith({
+                client: expect.any(S3Client),
+                params: {
+                    Bucket: 'bucket',
+                    Key: 'test.zip',
+                    Body: expect.any(Object),
+                },
             });
         });
-        it('correctly sets the parameters when calling getObject', async () => {
+        it('file directly have path', async () => {
             await uploadObject(new S3Client({}), 'bucket', 'path', 'test.zip', sdkStream);
-            expect(s3Mock.commandCalls(PutObjectCommand)[0].args[0].input).toEqual({
-                Bucket: 'bucket',
-                Key: 'path/test.zip',
-                Body: expect.any(Object),
+            expect(mocks.mockUpload).toHaveBeenCalledWith({
+                client: expect.any(S3Client),
+                params: {
+                    Bucket: 'bucket',
+                    Key: 'path/test.zip',
+                    Body: expect.any(Object),
+                },
             });
         });
     });

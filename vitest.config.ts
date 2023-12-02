c0306cb36a4d9c825f6003274a2febb9dd0afd9f
Wolfsin <choose.bc.wolf@gmail.com>
2023-12-02 22:48:31 +0900

    chore: Add coverage scripts to npm-test workflow

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    test: { globals: true },
+    test: {
+        globals: true,
+        coverage: {
+            reporter: ['text', 'json-summary', 'json'],
+        },
+    },
 });
